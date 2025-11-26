908eed37e5f3747381663076f5d4dc24ffa6d632
Anton Fröde <antonfroede@gmail.com>
2025-11-26 10:01:18 +0100

    "2 Buttons hinzugefügt"

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import fitz
+# import fitz
 from pathlib import Path
 from nicegui import ui
 
 
-ui.button("PDF importieren", on_click=lambda: ordner).props("icon=file_upload")
-ui.button("Sortierung starten", on_click=lambda: ausgabe_ordner).props(
-    "icon=play_arrow"
+ui.upload(on_upload=lambda e: ui.notify(f"Uploaded {e.file.name}")).classes(
+    "max-w-full"
 )
+
+ui.button("Sortierung starten", on_click=lambda: start_sorter).props("icon=play_arrow")
 
 ui.run()
 
@@ -15,41 +16,49 @@ ausgabe_ordner = Path(
     r"C:\Users\Keinp\Desktop\Coding\PDF automatisch sortieren nach RV-Nummer"
 )
 
-ausgabe_ordner.mkdir(exist_ok=True)
+
+def importiere_pdf():
+    list(ordner.glob("*pdf"))
 
 
-pdf_dateien = list(ordner.glob("*.pdf"))
+def start_sorter():
+    ui.notify("Sortierung gestartet")
 
 
-for datei in pdf_dateien:
-    with fitz.open(datei) as doc:
-        print(f"Verarbeite Datei: {datei.name} ({doc.page_count} Seiten)")
+# ausgabe_ordner.mkdir(exist_ok=True)
 
-        for page_num, page in enumerate(doc):
-            rect = fitz.Rect(25, 133, 119, 152)
-            text = page.get_textbox(rect)
-
-            print(f"Seite {page_num + 1}: RV-Nummer = {text}")
-
-            new_pdf = fitz.open()
-            new_pdf.insert_pdf(doc, from_page=page_num, to_page=page_num)
-
-            safe_name = "".join(c for c in text if c.isalnum() or c in ("-", "_"))
-            ausgabedatei = ausgabe_ordner / f"{safe_name}_{page_num + 1}.pdf"
-
-            new_pdf.save(ausgabedatei)
-            new_pdf.close()
+# pdf_dateien = list(ordner.glob("*.pdf"))
 
 
-alle_einzeln = sorted(ausgabe_ordner.glob("*.pdf"))
+# for datei in pdf_dateien:
+# with fitz.open(datei) as doc:
+# print(f"Verarbeite Datei: {datei.name} ({doc.page_count} Seiten)")
 
-gesamt_pdf = fitz.open()
-for datei in alle_einzeln:
-    print(f"Füge hinzu: {datei.name}")
-    with fitz.open(datei) as pdf:
-        gesamt_pdf.insert_pdf(pdf)
+# for page_num, page in enumerate(doc):
+# rect = fitz.Rect(25, 133, 119, 152)
+# text = page.get_textbox(rect)
 
-gesamt_pdf.save(ausgabe_ordner / "Sortiert.pdf")
-gesamt_pdf.close()
+# print(f"Seite {page_num + 1}: RV-Nummer = {text}")
 
-print("Fertig: Sortiert.pdf erstellt.")
+# new_pdf = fitz.open()
+# new_pdf.insert_pdf(doc, from_page=page_num, to_page=page_num)
+
+# safe_name = "".join(c for c in text if c.isalnum() or c in ("-", "_"))
+# ausgabedatei = ausgabe_ordner / f"{safe_name}_{page_num + 1}.pdf"
+
+# new_pdf.save(ausgabedatei)
+# new_pdf.close()
+
+
+# alle_einzeln = sorted(ausgabe_ordner.glob("*.pdf"))
+
+# gesamt_pdf = fitz.open()
+# for datei in alle_einzeln:
+# print(f"Füge hinzu: {datei.name}")
+# with fitz.open(datei) as pdf:
+# gesamt_pdf.insert_pdf(pdf)
+
+# gesamt_pdf.save(ausgabe_ordner / "Sortiert.pdf")
+# gesamt_pdf.close()
+
+# print("Fertig: Sortiert.pdf erstellt.")
